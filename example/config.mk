de5a994cfd31862a67f55426c56566a68fd66c43
时空自由民 <ufo281@outlook.com>
2025-03-08 23:17:27 +0800

    [V1.1.6]: 修改项目绝对路径

--- example/config.mk
+++ example/config.mk
@@ -2,17 +2,18 @@
 CC = gcc
 
 # 项目路径
-PRO_DIR = /home/wls/git/makefile_learn_note/example
+PRO_DIR := /home/wls/stm32/makefile_learn_note/example
 
 # 头文件包含目录
-INC_DIR = $(PRO_DIR)/Driver/inc
+INC_DIR := $(PRO_DIR)/Driver/inc
 
 # 编译生成目标文件目录
-BUILD_DIR = $(PRO_DIR)/build
+BUILD_DIR := $(PRO_DIR)/build
 
 # 定义编译选项
-CFLAGS = -Wall -Wextra -O2 -I $(INC_DIR)
+CFLAGS := -Wall -Wextra -O2 -I $(INC_DIR)
 
-BIN_DIR = $(PRO_DIR)/bin
+# := 赋值 只使用第一次定义的值
+BIN_DIR := $(PRO_DIR)/bin
 
 
