0dfa0788df03b808503279007cbd2cdd40e13eec
时空自由民 <ufo281@outlook.com>
2025-03-08 04:29:26 -0800

    [V1.1.1]:1.加入catgs（索引文件) 2.config.mk文件内加入项目路径变量

--- example/config.mk
+++ example/config.mk
@@ -1,15 +1,18 @@
 # 定义编译器
 CC = gcc
 
+# 项目路径
+PRO_DIR = /home/wls/git/makefile_learn_note/example
+
 # 头文件包含目录
-INC_DIR = /home/wls/git/makefile_learn_note/example/Driver/inc
+INC_DIR = $(PRO_DIR)/Driver/inc
 
 # 编译生成目标文件目录
-BUILD_DIR = /home/wls/git/makefile_learn_note/example/build
+BUILD_DIR = $(PRO_DIR)/build
 
 # 定义编译选项
 CFLAGS = -Wall -Wextra -O2 -I $(INC_DIR)
 
-BIN_DIR = /home/wls/git/makefile_learn_note/example/bin
+BIN_DIR = $(PRO_DIR)/bin
 
 
