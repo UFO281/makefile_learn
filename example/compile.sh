8a1bc1d5ac5ac4843662d00ed14579936572aded
时空自由民 <ufo281@outlook.com>
2025-03-08 04:47:23 -0800

    [V1.1.2]:修改编shell译脚本

--- example/compile.sh
+++ example/compile.sh
@@ -4,13 +4,13 @@
 
 # src/mat.c 依赖的源文件,
 # -I inc 指定mat.c里边包含的头文件路径
-gcc -c -o build/mat.o src/mat.c -I inc 
+gcc -c -o build/mat.o ./Driver/src/mat.c -I ./Driver/inc 
 
-gcc -c -o build/add.o src/add.c -I inc
+gcc -c -o build/add.o ./Driver/src/add.c -I ./Driver/inc
 
-gcc -c -o build/main.o app/main.c -I inc 
+gcc -c -o build/main.o app/main.c -I ./Driver/inc 
 
 # 将main.o 和其他几个.o,main函数里边调用到的 链接成可执行文件
-gcc -o test build/mat.o build/main.o build/add.o
+gcc -o bin/test build/mat.o build/main.o build/add.o
 
-./test
+./bin/test
